d45f13bd2125f3a1a42d4d85904741cf09205135
Bary Brown <barybrown@bary-browns-ibook-g4.local>
2010-08-27 23:20:23 -0500

    Chaining of commands with new

--- Demo/test.cpp
+++ Demo/test.cpp
@@ -13,9 +13,22 @@ int main(){
   //BEDfile bed2("Data/itree.bed");
   //importData(bed2, "asample");
 
-  Sample s("bsample");
-  Clique c(s, 5);
-  c.saveAsBED();
+  Chain c;
+  //Sample s1("bsample");
+  //Sample s2("bsample");
+  //c.flatten(c.clique(s, 2)).saveAsBED();
+  //c.merge(s,s).saveAsBED();
+
+  //chr8 40568857 405688824 .
+  //chr8 78193723 781937484 .
+  //chr8 85059549 850595744 .
+  //chr8 101641327 1016413524 .
+  //chr8 125556826 1255568374 .
+  //chr8 127407510 127407535 .
+  c.clique(c.merge(c.sample("bsample"),c.range("bsample", "chr8", 40568857, 227407535)), 4).saveAsBED();
+
+  //Clique c(s, 5);
+  //c.saveAsBED();
 
   //Merge(Sample("asample"), Sample("asample")).saveAsBED();
   //Sample s("asample");
--- Iterators/iterators.h
+++ Iterators/iterators.h
@@ -1,10 +1,64 @@
 #ifndef ITERS_H_
 #define ITERS_H_
 
+#include "iterator.h"
 #include "sample.h"
 #include "range.h"
 #include "merge.h"
 #include "flatten.h"
 #include "clique.h"
+
+namespace rivals {
+
+  class Chain {
+  public:
+    Iterator & sample(std::string sample){
+      Sample *s = new Sample(sample);
+      v.push_back(s);
+      return *s;
+    }
+    Iterator & sample(std::string sample, std::string chrom){
+      Sample *s = new Sample(sample, chrom);
+      v.push_back(s);
+      return *s;
+    }
+    Iterator & range(std::string sample, std::string chrom, Domain start, Domain stop){
+      Range *r = new Range(sample, chrom, start, stop);
+      v.push_back(r);
+      return *r;
+    }
+    Iterator & range(std::string sample, std::string chrom, Domain point){
+      Range *r = new Range(sample, chrom, point);
+      v.push_back(r);
+      return *r;
+    }
+    Iterator & merge(Iterator & a, Iterator & b){
+      Merge * m = new Merge(a, b);
+      v.push_back(m);
+      return *m;
+    }
+    Iterator & flatten(Iterator & a){
+      Flatten * f = new Flatten(a);
+      v.push_back(f);
+      return *f;
+    }
+    Iterator & clique(Iterator & a, Capacity minOverlap){
+      Clique * c = new Clique(a, minOverlap);
+      v.push_back(c);
+      return *c;
+    }
+    
+    ~Chain(){
+      for(size_t i = 0; i < v.size(); i++){
+	printf("deleting %zu\n", i);
+	delete v.at(i);
+      }
+    }
+    
+  private:
+    std::vector<Iterator *> v;
+  };
+
+}
 
 #endif
