badb8a428905c0e142db43a0b7b4c0753d93d1b4
Justin <jbrown23@gmail.com>
2010-09-17 00:08:19 -0500

    Added genomefile.h

---
+++ FileUtils/genomefile.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 Justin T. Brown
+ * All Rights Reserved
+ * EMAIL: run.intervals@gmail.com
+ *
+ * This file is part of Rivals.
+ *
+ * Rivals is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rivals is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Rivals.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GENOME_FILE_H_
+#define GENOME_FILE_H_
+
+#include "interval.h"
+#include <string>
+
+namespace rivals {
+  class GenomeFile {
+  public:
+    virtual ~GenomeFile(){}
+    virtual bool next(std::string & chr, Interval & c) = 0;
+  };
+  
+}
+
+#endif
