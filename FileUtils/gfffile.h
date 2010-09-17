a262ac5c44e6e30362704565c775225160561465
Justin <jbrown23@gmail.com>
2010-09-16 22:10:01 -0500

    Added Copyright information to each source file in Rivals Library.

--- FileUtils/gfffile.h
+++ FileUtils/gfffile.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2010 Justin T. Brown
+ * All Rights Reserved
+ * EMAIL: run.intervals@gmail.com
+ *
+ * This file is part of Rivals.
+ *
+ * Rivals is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rivals is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Rivals.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef GFF_FILE_H_
 #define GFF_FILE_H_
 
